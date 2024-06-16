7b6121150d80abdd62ec07633b1a1cd7df6f8121
pypsik1337 <vitya.nikitin.0101@mail.ru>
2024-06-16 22:51:02 +0600

    Update common.py

--- proekt/handlers/common.py
+++ proekt/handlers/common.py
@@ -1,3 +1,5 @@
+# В этом файле идет создание хендлеров на самые базовые команды, по типу приветствия и отмены какого либо действия
+# Импорты из библиотеки
 from aiogram import F, Router
 from aiogram.filters import Command, CommandStart
 from aiogram.filters import StateFilter
@@ -5,18 +7,21 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import default_state
 from aiogram.types import Message, ReplyKeyboardRemove
 
+# Импорт из файла с клавиатурой
 from keyboards.simple_row import  make_row_keyboard
 from handlers.credit_handlers import kb_bot
 
+# Создание роутера
 common_router = Router()
 
+# Хендлер реагирующий на команду /start который предоставляет пользователю клавиатуру
 @common_router.message(CommandStart())
 async def start_cmd(message: Message, state: FSMContext):
     await state.clear()
     await message.answer("Привет! Я Бот Нелепый. Расчитаю тебе платеж по кредиту ",
                          reply_markup= make_row_keyboard(kb_bot)
                         )
-
+# Хендлер на команду "отмена" когда пользователь не находится в каком либо состоянии
 @common_router.message(StateFilter(None), Command(commands="cancel"))
 @common_router.message(default_state, F.text.lower() == "отмена")
 async def cmd_cancel_no_state(message: Message, state: FSMContext):
@@ -26,7 +31,7 @@ async def cmd_cancel_no_state(message: Message, state: FSMContext):
         
     )
     
-
+# Хендлер на команду "отмена" когда пользватель находится в каком либо состоянии
 @common_router.message(Command(commands="cancel"))
 @common_router.message(F.text.lower() == "отмена")
 async def cmd_cancel(message: Message, state: FSMContext):
