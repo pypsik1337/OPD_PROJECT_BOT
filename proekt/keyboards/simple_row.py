fd2957d6d63b1f53e01db11783e6a29f2d9949ef
pypsik1337 <vitya.nikitin.0101@mail.ru>
2024-06-16 22:46:18 +0600

    Update simple_row.py

--- proekt/keyboards/simple_row.py
+++ proekt/keyboards/simple_row.py
@@ -1,13 +1,11 @@
+# В этом файле идет создание самой простой клавиатуры
+# :param items: список текстов для кнопок
+# :return: объект реплай-клавиатуры
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 kb_bot = ["Расчитать ежемесячный платеж", "Расчитать ипотечный платеж", "Досрочное погашение кредита"]
 kb_ipoteka = ["Да", "Нет"]
 
 def make_row_keyboard(items: list[str]) -> ReplyKeyboardMarkup:
-    """
-    Создаёт реплай-клавиатуру с кнопками в один ряд
-    :param items: список текстов для кнопок
-    :return: объект реплай-клавиатуры
-    """
     row = [KeyboardButton(text=item) for item in items]
     return ReplyKeyboardMarkup(keyboard=[row], resize_keyboard=True)
